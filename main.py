b9e47a2a5c860db6c2d3764269bd92f58739f9af
sumityadav1509 <sumityadav2005@gmail.com>
2020-11-19 16:12:47 +0000

    Python-coding-exercise-2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-
-print("Day 1 - Python Print Function")
-print("The function is declared like this:")
-print("print('What to print')")
+print("Day 1 - String Manipulation") 
+print("String Concatenation is done with the "+" sign ") 
+print("e.g. print('Hello' + 'world')") 
+print("New lines can be created with a backslash and n.")
