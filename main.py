46f66b7e8a832aec70274e0400a6504e5e53e4f6
sumityadav1509 <sumityadav2005@gmail.com>
2020-11-19 16:53:08 +0000

    Using length function

--- main.py
+++ main.py
@@ -1,5 +1,2 @@
 
-print("Day 1 - String Manipulation") 
-print("String Concatenation is done with the "+" sign ") 
-print("e.g. print('Hello' + 'world')") 
-print("New lines can be created with a backslash and n.")
+print(len(input("What is your name ?"))) 
